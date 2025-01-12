5c5effb98c414d4c4e52092d62acbf2fdd94410b
rofiqdoang <rozaqi.android@gmail.com>
2025-01-12 22:01:54 +0700

    test

---
+++ woi.java
@@ -0,0 +1,18 @@
+public abstract class woi {
+    private int id;
+    private String nama;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+
+}
